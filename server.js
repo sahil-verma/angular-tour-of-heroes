537a3ad9d5796fc3968b8e114c91e9b7737fb9c8
7059705091 <verma.sahil3232@gmail.com>
2018-04-25 16:14:24 -0400

    initial heroku commit

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app = express();
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started '+ port);
+console.log('server started on '+ port);
